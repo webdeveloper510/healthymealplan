f9f7b068d3092c2906604a15135d574b9406c3e3
Jivanysh <jivanyesh@gmail.com>
2017-10-03 22:15:40 +0530

    commented oAuth code

--- imports/startup/server/accounts/oauth.js
+++ imports/startup/server/accounts/oauth.js
@@ -1,13 +1,13 @@
-import { Meteor } from 'meteor/meteor';
-import { ServiceConfiguration } from 'meteor/service-configuration';
+// import { Meteor } from 'meteor/meteor';
+// import { ServiceConfiguration } from 'meteor/service-configuration';
 
-const OAuthSettings = Meteor.settings.private.OAuth;
+// const OAuthSettings = Meteor.settings.private.OAuth;
 
-if (OAuthSettings) {
-  Object.keys(OAuthSettings).forEach((service) => {
-    ServiceConfiguration.configurations.upsert(
-      { service },
-      { $set: OAuthSettings[service] },
-    );
-  });
-}
+// if (OAuthSettings) {
+//   Object.keys(OAuthSettings).forEach((service) => {
+//     ServiceConfiguration.configurations.upsert(
+//       { service },
+//       { $set: OAuthSettings[service] },
+//     );
+//   });
+// }
