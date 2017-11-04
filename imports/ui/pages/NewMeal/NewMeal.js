e35bf54afde2b053ca0c3d40a42f76bd18fde365
Jivanysh <jivanyesh@gmail.com>
2017-11-04 23:20:44 +0530

    fixed new meal textfield disabled

--- imports/ui/pages/NewMeal/NewMeal.js
+++ imports/ui/pages/NewMeal/NewMeal.js
@@ -4,15 +4,16 @@ import Grid from 'material-ui/Grid';
 
 import MealEditor from '../../components/MealEditor/MealEditor';
 
-const NewMeal = ({ history, popTheSnackbar }) => (
+const NewMeal = ({ history, popTheSnackbar, newMeal }) => (
   <div>
     <Grid container className="NewMeal SideContent SideContent--spacer-2x--horizontal">
-      <MealEditor popTheSnackbar={popTheSnackbar} history={history} />
+      <MealEditor newMeal={true} popTheSnackbar={popTheSnackbar} history={history} />
     </Grid>
   </div>
 );
 
 NewMeal.propTypes = {
+  newMeal: PropTypes.bool.isRequired,
   history: PropTypes.object.isRequired,
   popTheSnackbar: PropTypes.func.isRequired,
 };
