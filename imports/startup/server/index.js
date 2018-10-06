073d52a337de7c98cfcaee769cee1986a418fcf7
Jivanysh <jivanyesh@gmail.com>
2018-10-06 12:45:15 +0530

    add jobs logging

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,7 +1,9 @@
 import './accounts';
 import './api';
 import './email';
+
 import Jobs from '../../api/Jobs/Jobs';
+// import Subscriptions from '../../api/Subscriptions/Subscriptions';
 
 
 import './jobs';
@@ -22,6 +24,8 @@ import './jobs/disableDiscountJob';
 import './jobs/sendFirstRecoveryEmailJob';
 
 import '../../modules/server/authorize/webhooks/';
+// import calculateSubscriptionCost from '../../modules/server/billing/calculateSubscriptionCost';
+// import createSubscriptionFromCustomerProfile from '../../modules/server/authorize/createSubscriptionFromCustomerProfile';
 
 S3 = {};
 
@@ -31,3 +35,49 @@ S3.config = {
   bucket: 'vittle-new',
 };
 
+
+// const cost = calculateSubscriptionCost({});
+
+Jobs.setLogStream(process.stdout);
+
+// THIS IS TO BE USED IN AN EMERGENCY
+// MANUAL SUBSCRIPTION EDIT CODE BELOW
+
+// const restart = Jobs.restartJobs(['vQHrmWAY8ingroAm5']);
+
+// console.log(restart);
+
+
+// console.log(cost);
+
+// Subscriptions.update({
+//   customerId: 'tKmQ5BZSDsh5jLrJg',
+// }, {
+//   $set: {
+//     amount: cost.actualTotal,
+//     subscriptionItems: cost.lineItems,
+//   },
+
+// });
+
+
+// Subscriptions.update({
+//   customerId: 'zA2eKzffCuXwqETWY',
+// }, {
+//   $set: {
+//     amount: cost.actualTotal,
+//     subscriptionItems: cost.lineItems,
+//   },
+
+// });
+
+// const syncCreateSubscriptionFromCustomerProfile = Meteor.wrapAsync(createSubscriptionFromCustomerProfile);
+
+// const createSubscriptionFromCustomerProfileRes = syncCreateSubscriptionFromCustomerProfile(
+//   '1935114800',
+//   '1948695142',
+//   '2018-09-29',
+//   69.61,
+// );
+
+// console.log(createSubscriptionFromCustomerProfileRes);
