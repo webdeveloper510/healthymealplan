0b0bfbf531ebb28ce63ab0abbd3c9b1f1bfa5ac5
Jivanysh Sohoni <jivanyesh@gmail.com>
2019-05-04 15:28:54 +0530

    remove manual subscription charge commented code

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,10 +2,6 @@ import './accounts';
 import './api';
 import './email';
 
-import Jobs from '../../api/Jobs/Jobs';
-// import Subscriptions from '../../api/Subscriptions/Subscriptions';
-
-
 import './jobs';
 import './jobs/setSubscriptionActive';
 import './jobs/setSubscriptionActiveCard';
@@ -30,8 +26,6 @@ import '../../modules/server/authorize/webhooks/';
 
 import './jobs/masterBillingJob';
 import './jobs/slaveChargeJob';
-// import calculateSubscriptionCost from '../../modules/server/billing/calculateSubscriptionCost';
-// import createSubscriptionFromCustomerProfile from '../../modules/server/authorize/createSubscriptionFromCustomerProfile';
 
 S3 = {};
 
@@ -41,48 +35,4 @@ S3.config = {
   bucket: 'vittle-new',
 };
 
-// const cost = calculateSubscriptionCost({});
-
  // Jobs.setLogStream(process.stdout);
-
-// THIS IS TO BE USED IN AN EMERGENCY
-// MANUAL SUBSCRIPTION EDIT CODE BELOW
-
-// const restart = Jobs.restartJobs(['vQHrmWAY8ingroAm5']);
-
-// console.log(restart);
-
-
-// console.log(cost);
-
-// Subscriptions.update({
-//   customerId: 'tKmQ5BZSDsh5jLrJg',
-// }, {
-//   $set: {
-//     amount: cost.actualTotal,
-//     subscriptionItems: cost.lineItems,
-//   },
-
-// });
-
-
-// Subscriptions.update({
-//   customerId: 'zA2eKzffCuXwqETWY',
-// }, {
-//   $set: {
-//     amount: cost.actualTotal,
-//     subscriptionItems: cost.lineItems,
-//   },
-
-// });
-
-// const syncCreateSubscriptionFromCustomerProfile = Meteor.wrapAsync(createSubscriptionFromCustomerProfile);
-
-// const createSubscriptionFromCustomerProfileRes = syncCreateSubscriptionFromCustomerProfile(
-//   '1935114800',
-//   '1948695142',
-//   '2018-09-29',
-//   69.61,
-// );
-
-// console.log(createSubscriptionFromCustomerProfileRes);
