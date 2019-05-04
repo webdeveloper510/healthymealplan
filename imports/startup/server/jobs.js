36461ac0df0733b4a9abb1db6f931e75a88eb377
Jivanysh Sohoni <jivanyesh@gmail.com>
2019-05-04 15:48:16 +0530

    remove job events code

--- imports/startup/server/jobs.js
+++ imports/startup/server/jobs.js
@@ -4,15 +4,3 @@ import Jobs from '../../api/Jobs/Jobs';
 Jobs.startJobServer();
 
 // Jobs.setLogStream(process.stdout.error);
-
-// Jobs.events.on('call', (msg) => {
-//   if (msg.method === 'jobDone' && error == null) {
-//     console.log(`Job${  msg.params[0]  }finished!`);
-//   }
-// });
-
-
-// Jobs.events.on('error', (msg) => {
-//   console.log(`Job${  msg.params[0]  } error!`);
-//   console.log(msg.error);
-// });
